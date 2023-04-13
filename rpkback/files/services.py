78f1ebe613ad49572ff402b1d8db4aec4ec12341
saladware <saladware46@gmail.com>
2023-04-13 20:03:12 +0300

    bug fix

--- rpkback/files/services.py
+++ rpkback/files/services.py
@@ -9,8 +9,4 @@ from rpkback.storage import FileStorage
 async def get_package_version_archive(session: AsyncSession, package: str, version: str, storage: FileStorage):
     version = await get_package_version(session, package, version)
     data = await storage.download_file(version.url)
-    with open('file.tar', 'w+b') as file:
-        file.write(data)
-    print(data)
-    print(file.read())
     return Response(data, media_type='application/tar+gzip')
